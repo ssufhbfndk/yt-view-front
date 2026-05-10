3f27cc89d2daa5a2bbf0cf885253d4b328a634a8
zain khan <ssufhbfndk@gmail.com>
2026-05-10 17:26:28 +0500

    floder update

--- src/com/user/UserProfile.js
+++ src/com/user/UserProfile.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import { useAuth } from "../context/AuthContext";
+import { useAuth } from "../../context/AuthContext";
 import "./UserProfile.css"; // Import custom CSS
 import { FaCoins } from "react-icons/fa"; // Import coin icon
 
